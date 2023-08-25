45822bfc015c2e7d779215142198596f71950060
Hanss Espinoza <hanssabinadie@gmail.com>
2023-08-24 22:13:44 -0600

    add routing app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
@@ -10,6 +12,8 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraficalComponent } from './pages/grafical/grafical.component';
 
 @NgModule({
   declarations: [
@@ -22,9 +26,12 @@ import { FooterComponent } from './shared/footer/footer.component';
     SidebarComponent,
     HeaderComponent,
     FooterComponent,
+    ProgressComponent,
+    GraficalComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
