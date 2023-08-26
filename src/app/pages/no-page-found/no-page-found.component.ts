681f63fa47395d3c69155607225670c34723ea52
Hanss Espinoza <hanssabinadie@gmail.com>
2023-08-25 20:03:41 -0600

    no page foun mock

--- src/app/pages/no-page-found/no-page-found.component.ts
+++ src/app/pages/no-page-found/no-page-found.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-no-page-found',
   templateUrl: './no-page-found.component.html',
-  styles: [
-  ]
+  styleUrls: ['./no-page-found.component.css']
 })
 export class NoPageFoundComponent {
+  // Forma tradicional
+  // public year = new Date().getFullYear();
 
+  // Signal
+  public year = signal<number>(new Date().getFullYear());
 }
