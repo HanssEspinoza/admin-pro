c2883a84db575f1c2ce30c38ef69c5a96ea8a712
Hanss Espinoza <hanssabinadie@gmail.com>
2023-09-01 21:13:09 -0600

    ci: add tailwindcss

--- src/app/features/admin/admin-layout/admin-layout.component.ts
+++ src/app/features/admin/admin-layout/admin-layout.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'admin-layout',
   templateUrl: './admin-layout.component.html',
-  styles: [
-  ]
+  styleUrls: ['../../../../assets/css/style.css']
 })
 export class AdminLayoutComponent {
 
