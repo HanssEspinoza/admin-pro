077d9dd9af0dea129c3acb2b619b62f3af6b8470
Hanss Espinoza <hanssabinadie@gmail.com>
2023-09-04 22:19:02 -0600

    feat: add eye icon

--- src/app/features/public/pages/login/components/login-form/login-form.component.ts
+++ src/app/features/public/pages/login/components/login-form/login-form.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+import {
+  IconDefinition,
+  faEye,
+  faEyeSlash,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'login-form',
   templateUrl: './login-form.component.html',
 })
-export class LoginFormComponent {}
+export class LoginFormComponent {
+  public faEye = signal<IconDefinition>(faEye);
+  public faEyeSlash = signal<IconDefinition>(faEyeSlash);
+  public showPassword = signal<boolean>(false);
+}
--- src/app/features/public/pages/login/login.module.ts
+++ src/app/features/public/pages/login/login.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { LoginFormComponent } from './components';
@@ -15,7 +17,8 @@ import { BtnComponent } from '@shared/components';
   imports: [
     CommonModule,
     LoginRoutingModule,
-    BtnComponent
+    BtnComponent,
+    FontAwesomeModule,
   ]
 })
 export class LoginModule { }
