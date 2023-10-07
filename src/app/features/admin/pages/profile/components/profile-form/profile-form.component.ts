ef2a458c645aa498942b2aeee408467e0bad4808
Hanss Espinoza <hanssabinadie@gmail.com>
2023-10-06 21:03:21 -0600

    feat: profile service

--- src/app/features/admin/pages/profile/components/profile-form/profile-form.component.ts
+++ src/app/features/admin/pages/profile/components/profile-form/profile-form.component.ts
@@ -1,7 +1,14 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, DestroyRef, computed, inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { AuthService } from '@features/public';
+import { ProfileService } from '@features/admin/services';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ToastService } from '@core/services';
+import {
+  faCheckCircle,
+  faCircleXmark,
+} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'admin-profile-form',
@@ -15,6 +22,9 @@ export class ProfileFormComponent {
 
   private authService = inject(AuthService);
   private fb = inject(FormBuilder);
+  private profileService = inject(ProfileService);
+  private destroyRef = inject(DestroyRef);
+  private toastService = inject(ToastService);
 
   public user = computed(() => this.authService.CurrentUser());
 
@@ -23,13 +33,41 @@ export class ProfileFormComponent {
   }
 
   public onSave() {
-    console.log(this.profileForm.value);
+    if (this.profileForm.valid) {
+      this.profileService
+        .editProfile(this.profileForm.value, this.user()!.user_id)
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe({
+          next: ({ reply, message }) =>
+            this.toastService.show({
+              color: 'success',
+              message,
+              icon: faCheckCircle,
+              duration: 4000,
+            }),
+          error: (message) => {
+            console.log(message);
+            this.toastService.show({
+              color: 'error',
+              message,
+              icon: faCircleXmark,
+              duration: 4000,
+            });
+          },
+        });
+    }
   }
 
   private buildForm() {
     this.profileForm = this.fb.group({
-      first_name: [this.user()!.first_name, [Validators.required]],
-      last_name: [this.user()!.last_name, [Validators.required]],
+      first_name: [
+        this.user()!.first_name,
+        [Validators.required, Validators.minLength(2)],
+      ],
+      last_name: [
+        this.user()!.last_name,
+        [Validators.required, Validators.minLength(2)],
+      ],
       email: [this.user()!.email, [Validators.required, Validators.email]],
     });
   }
